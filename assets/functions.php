5df7fca5596a2fc3bd7e3194c6858d77c351d77e
tekelleher <tekelleher@umass.edu>
2017-08-22 22:23:32 -0400

    Idea for how to generate forms with a php function

---
+++ assets/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+
+Array of Key Arrays
+
+So:
+
+Form (1)
+    id -> newuser
+    method -> post
+    ref -> new_user.php
+Text (2)
+    type -> text
+    name -> username
+    value -> username
+    required -> 
+Text (3)
+    type -> text
+    name -> umassemail
+    value -> UMASS Email
+    required -> true
+
+
+Then...
+
+<input type="$type" class="form-control" ##GO THROUGH REST OF KEYS />
+
+?>
